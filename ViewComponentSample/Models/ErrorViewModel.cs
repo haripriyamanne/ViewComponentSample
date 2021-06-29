2558ca589dc04015ff38095ca290b3f28fdf8fc5
haripriyamanne <haripriyamanne4@gmail.com>
2021-06-29 20:57:02 +0530

    Add project files.

---
+++ ViewComponentSample/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ViewComponentSample.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
